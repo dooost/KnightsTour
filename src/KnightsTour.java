0e3dc8f9bcd56fe46a396d9da29a73182216021e
Basir Doost <>
2019-04-22 16:41:11 +0200

    Added sort of a UI

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +18,24 @@ public class KnightsTour {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        KTSolver kt = new KTSolver(8, 0, 0);
+        int n;
+        int row;
+        int col;
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter board length:");
+        n = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        System.out.println("Enter starting row [0,n-1]:");
+        row = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        System.out.println("Enter starting column [0,n-1]:");
+        col = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        KTSolver kt = new KTSolver(n, row, col);
         kt.solve();
     }
     
